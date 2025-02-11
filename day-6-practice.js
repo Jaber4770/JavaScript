055a40b66fa5fb680a207f02c65aaa2af25604fb
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-02-11 21:05:39 +0200

    day 6 done

--- day-6-practice.js
+++ day-6-practice.js
@@ -8,11 +8,11 @@ console log the final array */
 let fruits = ["mango", "banana", "jackfruits", "painapple", "lichi"];
 console.log(fruits[2]);
 console.log(fruits);
-fruits[2]="jambura";
+fruits[2] = "jambura";
 console.log(fruits);
 
 
- /* 2. Add or remove elements
+/* 2. Add or remove elements
 Declare an array of 3 tourist destinations
 Add a new tourist destination to your tourist array
 Add two more to your array
@@ -20,8 +20,8 @@ Remove the last tourist destination you have added
 display the final array as output */
 
 let places = ["sajek", "bandarban", "sondarban"];
-console.log("Places: ",places);
-places.push("kataban","jhawban");
+console.log("Places: ", places);
+places.push("kataban", "jhawban");
 console.log(places);
 places.pop();
 console.log(places);
@@ -33,11 +33,12 @@ Create an array of books containing different book.
 Use the includes method to check if the array contains a javascript book.
 Print a message to the console indicating whether the element is present in the array or not.  */
 
-
-
-
-
-
+let books = ["math", "physics", "chemistry", "biology"];
+let js = books.includes("javascript");
+console.log("JavaScript book is available: ", js);
+books.push("javascript");
+let jsbook = books.includes("javascript");
+console.log("JavaScript book is available: ", jsbook);
 
 
 /* 4. Checking if it's an Array
@@ -47,8 +48,9 @@ Now use isArray to check if each variable is an array.
 Print a message to the console indicating whether each variable is an array or not.
  */
 
-
-
+let array = ["potato", "avocado", "banana", "lichi"];
+let arrayCheck = Array.isArray(array);
+console.log("This is an array: ", arrayCheck);
 
 
 
@@ -57,3 +59,19 @@ Instructions:
 Create two arrays of your choice.
 Use the concat method to combine the two arrays into a new array.
 Print both the original arrays and the combined array using console.log(). */
+
+
+let food = ["fruits", "vagatable", "spinich"];
+let other = ["fruits2", "vagatable2", "spinich2"];
+let newArray = food.concat(other);
+console.log(newArray);
+
+
+
+
+
+
+
+
+
+
