be05ec2d982b81429d96708b8b2a93fdbb6dfef7
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-02-13 23:45:29 +0200

    conversion going on

--- day-9-simple-problem-3.js
+++ day-9-simple-problem-3.js
@@ -1,9 +1,38 @@
 // Inch to feet
 
-function inchToFeet(inch){
-    const feet = inch /12;
+function inchToFeet(inch) {
+    const feet = inch / 12;
     return feet;
 }
 
 const inchToFeetResult = inchToFeet(36);
-console.log("Feet: ",inchToFeetResult);
+console.log("Feet: ", inchToFeetResult);
+
+
+// Milest to KM
+
+function mileToKm(mile) {
+    const km = mile * 1.60934;
+    return km;
+}
+console.log("Km: ", mileToKm(10));
+
+
+
+// km to miles
+
+function kmToMiles(km) {
+    const miles = km * 0.621371;
+    return miles;
+}
+
+console.log("Miles: ", kmToMiles(20));
+
+
+
+
+
+
+
+
+
