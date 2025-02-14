582fea637dade5fffb04972cf2efe2687eae9152
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-02-14 21:37:17 +0200

    learing

--- day-9-simple-problem-3.js
+++ day-9-simple-problem-3.js
@@ -30,14 +30,56 @@ console.log("Miles: ", kmToMiles(20));
 
 // leap year
 
-function leapYear(year){
-    if(year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
+function leapYear(year) {
+    if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
         return ("This is a Leap Year");
     }
     return "This is not a leap year";
 }
 
 console.log(leapYear(2020));
+
+
+// array sum > avarage
+function oddAvarage(array) {
+    let OddSum = 0;
+    let oddArray = [];
+    let OddSumAvg = 0;
+    for (let element of array) {
+        if (element % 2 !== 0) {
+            oddArray.push(element);
+        }
+    }
+    for (let item of oddArray) {
+        OddSum = OddSum + item;
+        OddSumAvg = OddSum / oddArray.length;
+    }
+    console.log(oddArray)
+    return OddSumAvg;
+}
+
+let numberOfArrya = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1001];
+const oddAvage = oddAvarage(numberOfArrya);
+console.log(oddAvage);
+
+
+// Find the minimum number of an array.
+
+let numbers = [13, 24, 53, 64, 75, 86, 97, 87, 56, 36, 33, 24, 33, 1];
+
+function findTheMinimum(array) {
+    let minimum = array[0];
+    for( numb of array){
+        if(numb < minimum){
+            minimum = numb;
+        }
+    }
+    return minimum;
+}
+
+const minimumbNumber = findTheMinimum(numbers);
+console.log(minimumbNumber);
+
 
 
 
